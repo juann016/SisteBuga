975bdc03334117eaee902c6409f7edbcc744212e
juann016 <felif3811@gmail.com>
2025-02-23 10:16:30 -0500

    Primer commit

---
+++ form.js
@@ -0,0 +1,15 @@
+
+function showWelcome() {
+  alert("Mensaje enviado correctamente ")
+
+  // Redirigir a la página principal después de 3 segundos
+  setTimeout(() => {
+      window.location.href = "index.html"; // Cambia "index.html" por la URL de tu página principal
+  }, 3000);
+}
+
+
+
+
+
+     
